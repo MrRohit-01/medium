c8838157ee73fd2b50d3c8e4c61724a3b0e000a6
Rohit kumar barada <rohitbarada@gmail.com>
2024-05-08 01:45:58 +0530

    one step ahead

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -4,11 +4,12 @@ const user = new Hono()
 
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
-const prisma = new PrismaClient({
-  datasourceUrl: process.env.DATABASE_URL,
-}).$extends(withAccelerate())
 
 user.post(" /user/signup", (c) => {
+  const prisma = new PrismaClient({
+    //@ts-ignore
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
   return c.text("signup routes")
 })
 user.post(" /user/signin", (c) => {
@@ -27,5 +28,5 @@ user.get(" /blog/:id", (c) => {
 })
 user.get(" blog/bulk", (c) => {
   return c.text("all blogroutes")
-}) 
+})
 export default user;
