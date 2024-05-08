f5aefabeb7d60644ebe0eb31e1ec63502c606929
Rohit kumar barada <rohitbarada@gmail.com>
2024-05-08 23:52:18 +0530

    added signup & signin routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,6 @@
 import { Hono } from 'hono'
 import user from './routes/user';
-import * as dotenv from 'dotenv';
-dotenv.config();
+
 
 const app = new Hono()
 
--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,32 +1,79 @@
 import { Hono } from 'hono'
-
-const user = new Hono()
+import { decode, sign, verify } from 'hono/jwt'
+const user = new Hono<{
+  Bindings:{
+    DATABASE_URL :string,
+    JWT_SECRET :string
+  }
+}>()
 
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
+import { Bindings } from 'hono/types'
 
-user.post(" /user/signup", (c) => {
+user.post(" /user/signup",async (c) => {
+
   const prisma = new PrismaClient({
-    //@ts-ignore
     datasourceUrl: c.env.DATABASE_URL,
   }).$extends(withAccelerate())
-  return c.text("signup routes")
+  
+  const body =await c.req.json(); 
+const response =await prisma.user.create({
+  data:{
+    email:body.email,
+    password:body.password
+  },
+  select:{
+    email:true
+  }
+ })
+const token = await sign({email:response.email}, c.env.JWT_SECRET)
+  return c.json({jwt:token});
+  
 })
-user.post(" /user/signin", (c) => {
-  return c.text("signin routes")
+
+
+user.post(" /user/signin",async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+  
+  const body =await c.req.json(); 
+
+  const user =await prisma.user.findUnique({
+ 
+    where:{
+      email:body.email
+    }
+   })
+   if(!user){
+    return c.text("error, user "+body.email+" doesn't exist,sign up first")
+   }
+   const token = await sign({email:body.email}, c.env.JWT_SECRET)
+   return c.json({jwt:token});
+
 })
+
+
 user.post(" /blog", (c) => {
   return c.text("post blog routes")
 })
+
+
 user.put(" /blog", (c) => {
   return c.text("blog update routes")
 })
+
+
 user.get(" /blog/:id", (c) => {
   const id = c.req.param('id')
   console.log(id)
   return c.text("get blog by id routes")
 })
+
+
 user.get(" blog/bulk", (c) => {
   return c.text("all blogroutes")
 })
+
 export default user;
