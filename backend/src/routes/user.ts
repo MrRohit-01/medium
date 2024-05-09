955b93d00c63754dc1659d19a70abd0dbcbffbb6
Rohit kumar barada <rohitbarada@gmail.com>
2024-05-09 22:45:51 +0530

    update

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,9 +1,9 @@
 import { Hono } from 'hono'
 import { decode, sign, verify } from 'hono/jwt'
 const user = new Hono<{
-  Bindings:{
-    DATABASE_URL :string,
-    JWT_SECRET :string
+  Bindings: {
+    DATABASE_URL: string,
+    JWT_SECRET: string
   }
 }>()
 
@@ -11,46 +11,46 @@ import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
 import { Bindings } from 'hono/types'
 
-user.post(" /user/signup",async (c) => {
+user.post(" /user/signup", async (c) => {
 
   const prisma = new PrismaClient({
     datasourceUrl: c.env.DATABASE_URL,
   }).$extends(withAccelerate())
-  
-  const body =await c.req.json(); 
-const response =await prisma.user.create({
-  data:{
-    email:body.email,
-    password:body.password
-  },
-  select:{
-    email:true
-  }
- })
-const token = await sign({email:response.email}, c.env.JWT_SECRET)
-  return c.json({jwt:token});
-  
+
+  const body = await c.req.json();
+  const response = await prisma.user.create({
+    data: {
+      email: body.email,
+      password: body.password
+    },
+    select: {
+      email: true
+    }
+  })
+  const token = await sign({ email: response.email }, c.env.JWT_SECRET)
+  return c.json({ jwt: token });
+
 })
 
 
-user.post(" /user/signin",async (c) => {
+user.post(" /user/signin", async (c) => {
   const prisma = new PrismaClient({
     datasourceUrl: c.env.DATABASE_URL,
   }).$extends(withAccelerate())
-  
-  const body =await c.req.json(); 
 
-  const user =await prisma.user.findUnique({
- 
-    where:{
-      email:body.email
+  const body = await c.req.json();
+
+  const user = await prisma.user.findUnique({
+
+    where: {
+      email: body.email
     }
-   })
-   if(!user){
-    return c.text("error, user "+body.email+" doesn't exist,sign up first")
-   }
-   const token = await sign({email:body.email}, c.env.JWT_SECRET)
-   return c.json({jwt:token});
+  })
+  if (!user) {
+    return c.text("error, user " + body.email + " doesn't exist,sign up first")
+  }
+  const token = await sign({ email: body.email }, c.env.JWT_SECRET)
+  return c.json({ jwt: token });
 
 })
 
