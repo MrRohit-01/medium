ec7c6463a127b8e6ffea4757723dbe37668564db
Rohit kumar barada <rohitbarada@gmail.com>
2024-05-16 23:40:07 +0530

    working on signup fetch function

--- frontend/src/components/button.tsx
+++ frontend/src/components/button.tsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React from 'react';
 
 interface Inputbuttonprops {
@@ -7,11 +8,18 @@ password:string
 username?:string
 }
 function ButtonData({button,email,password,username}:Inputbuttonprops) {
+  const dataFetch=async()=>{
+    const data =await axios.post("https://backend.rohitkumarbarada.workers.dev/api/v1",{
+    email:email,
+    name:username,
+    password:password
+    })
+   alert(data);
+  }
   return (
     <div className={"p-4 pt-5"}>
-   <button className="w-96 bg-black text-white border rounded-md py-1.5 px-2 text-md font-medium">{button}</button>
+   <button className="w-96 bg-black text-white border rounded-md py-1.5 px-2 text-md font-medium" onClick={()=>{dataFetch}} >{button}</button>
     </div>
   );
 }
-
 export default ButtonData;
